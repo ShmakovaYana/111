bbce462111f0042092b05042beda4086cd5336a0
ShmakovaYana <yanaaaaaa1998@mail.ru>
2020-02-14 11:59:44 +0300

    commit

--- src/main/java/Serialization.java
+++ src/main/java/Serialization.java
@@ -1,22 +1,30 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Serializable;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
 import java.util.List;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-public class Serialization implements Serializable {
+public class Serialization {
 
-    public static void SerializationWrite(List<Food> food, String nameFile)throws IOException, ClassNotFoundException{
-        Path path = Path 
-        try (FileWriter fileWriter = new FileWriter(Files.newInputStream(path))){
-            fileWriter.writeObject(animalList);
-        }
+    public static void SerializationWrite(List<Animal> ListAnimal, String nameFile)throws IOException, ClassNotFoundException{
+        Path path = Paths.get(nameFile);
+
+            try (ObjectOutputStream outputStream =
+                        new ObjectOutputStream(Files.newOutputStream(path))) {
+                outputStream.writeObject(ListAnimal);
+            }
     }
 
-    void SerializationRead(){
-
+    public static List<Animal> SerializationRead(Animal animal, String nameFile)throws IOException, ClassNotFoundException{
+        List<Animal> newList;
+        Path path = Paths.get(nameFile);
+           try (ObjectInputStream inputStream =
+                        new ObjectInputStream(Files.newInputStream(path))) {
+               newList = (List<Animal>) inputStream.readObject();
+           }
+        return newList;
     }
 
 }
---
+++ src/test/java/SerializationTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SerializationTest {
+
+    @Test
+    public void serializationWrite() {
+    }
+
+    @Test
+    public void serializationRead() {
+    }
+}
